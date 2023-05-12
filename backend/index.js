96e26c13836e3f5dd9881c2daa638e7ab10e5238
Rahul Rudra <rahulrudra665@gmail.com>
2023-05-12 11:03:52 +0600

    database added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
-
+require("dotenv").config();
 const port = 5000;
-
 const app = express();
+
 app.use(cors());
 
 // parse application/x-www-form-urlencoded
@@ -12,6 +13,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ga4zxwv.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
